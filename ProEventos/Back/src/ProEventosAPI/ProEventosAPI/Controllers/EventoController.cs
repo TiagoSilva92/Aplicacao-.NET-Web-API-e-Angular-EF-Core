3194bd079cf5ccdbedb81c9c1985976a543ead63
Tiago Silva <tiagodasilva360@hotmail.com>
2023-07-27 18:27:54 -0300

    Criacao do projeto
    Criacao do projeto

---
+++ ProEventos/Back/src/ProEventosAPI/ProEventosAPI/Controllers/EventoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventosAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "value";
+        }
+    }
+}
